86155cab408607dc6ecd7613f98421f735bc3ce2
AvilaEducation <marcoantonio.avilatapia@bue.edu.ar>
2025-11-12 21:06:02 -0300

    agregando base de datos al proyecto

---
+++ spring-boot/clase3_base_de_datos/src/main/java/com/techlab/demo/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.techlab.demo.repository;
+
+import com.techlab.demo.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Producto, Long> {
+
+}
